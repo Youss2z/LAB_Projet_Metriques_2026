e1255022697c8966058d796f2f722fe3b2a6591e
You2Ss926 <youxssef94200@gmail.com>
2026-05-19 22:17:15 +0200

    Ajout fichiers projet

--- app.py
+++ app.py
@@ -12,7 +12,60 @@ def hello_world():
 
 
 
+@app.route("/contact")
+def MaPremiereAPI():
+    return render_template("contact.html")
 
+
+
+
+def api_paris():
+    
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    temps = data.get("hourly", {}).get("temperature_2m", [])
+
+    n = min(len(times), len(temps))
+    result = [
+        {"datetime": times[i], "temperature_c": temps[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme")
+def monhistogramme():
+    return render_template("histogramme.html")
+
+@app.get("/vent_marseille")
+def api_vent_marseille():
+    # Coordonnées géographiques de Marseille
+    url = "https://api.open-meteo.com/v1/forecast?latitude=43.2965&longitude=5.3698&hourly=wind_speed_10m"
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    winds = data.get("hourly", {}).get("wind_speed_10m", [])
+
+    n = min(len(times), len(winds))
+    result = [
+        {"datetime": times[i], "wind_speed": winds[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+@app.route("/atelier")
+def monatelier():
+    return render_template("atelier.html")
 
 # Ne rien mettre après ce commentaire
     
